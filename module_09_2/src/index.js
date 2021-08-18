48e300de85d470a5ea5bed180ceae4d5a45f805e
vadym pichurin <vadym.pichurin@leaseplan.com>
2021-08-18 10:49:24 +0300

    added webpack

---
+++ module_09_2/src/index.js
@@ -0,0 +1,9 @@
+import fetchAllUsers from "./js/api";
+
+import './css/common.scss';
+import './css/button.scss';
+
+
+console.log('Hi from index.js');
+
+fetchAllUsers();
---
+++ module_09_2/src/js/api.js
@@ -0,0 +1,5 @@
+const fetchUsers = () => {
+    console.log('All users');
+}
+
+export default fetchUsers;
